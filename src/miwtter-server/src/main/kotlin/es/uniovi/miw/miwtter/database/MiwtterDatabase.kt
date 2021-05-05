027ba2932b5d72e193e9a01e45f3b4f2c8a87eb0
thewillyhuman <colunga91@gmail.com>
2021-05-05 19:32:15 +0200

    Protofile updated and added to the android app client

--- src/miwtter-server/src/main/kotlin/es/uniovi/miw/miwtter/database/MiwtterDatabase.kt
+++ src/miwtter-server/src/main/kotlin/es/uniovi/miw/miwtter/database/MiwtterDatabase.kt
@@ -1,5 +1,6 @@
 package es.uniovi.miw.miwtter.database
 
+import es.uniovi.miw.miwtter.Miwtter
 import es.uniovi.miw.miwtter.database.domain.Post
 import es.uniovi.miw.miwtter.database.domain.User
 import es.uniovi.miw.miwtter.features.authentication.login.LoginData
@@ -7,21 +8,17 @@ import es.uniovi.miw.miwtter.features.feed.CreatePostData
 
 interface MiwtterDatabase {
 
-    fun registerUser(user: RegisterData): Boolean
+    fun registerUser(request: Miwtter.RegisterUserRequest): Miwtter.RegisterUserResponse
 
-    fun loginUser(user: LoginData): Boolean
+    fun loginUser(request: Miwtter.LoginUserRequest): Miwtter.LoginUserResponse
 
-    fun findUserByFreeText(freeText: String): List<User>
+    fun findUserByFreeText(request: Miwtter.FindUserRequest): Miwtter.FindUserResponse
 
-    fun createPost(post: CreatePostData): Boolean
+    fun createPost(request: Miwtter.CreatePostRequest): Miwtter.CreatePostResponse
 
-    fun addLikeToPost(actorUsername: String, postId: String)
+    fun addLikeToPost(request: Miwtter.LikePostRequest): Miwtter.LikePostResponse
 
-    fun removeLike(actorUsername: String, postId: String)
+    fun removeLike(request: Miwtter.RemoveLikeRequest): Miwtter.RegisterUserResponse
 
-    fun getFeedForUser(username: String): List<Post>
-
-    fun getUser(username: String): User?
-
-    fun getPost(postId: String): Post?
+    fun getFeedForUser(request: Miwtter.GetFeedRequest): Miwtter.GetFeedResponse
 }
--- src/miwtter-server/src/main/kotlin/es/uniovi/miw/miwtter/database/adapters/MiwtterDatabaseRedisAdapter.kt
+++ src/miwtter-server/src/main/kotlin/es/uniovi/miw/miwtter/database/adapters/MiwtterDatabaseRedisAdapter.kt
@@ -2,126 +2,44 @@ package es.uniovi.miw.miwtter.database.adapters
 
 import es.uniovi.miw.miwtter.Miwtter
 import es.uniovi.miw.miwtter.database.MiwtterDatabase
-import es.uniovi.miw.miwtter.database.domain.Post
-import es.uniovi.miw.miwtter.database.domain.User
-import es.uniovi.miw.miwtter.features.authentication.login.LoginData
-import es.uniovi.miw.miwtter.features.feed.CreatePostData
 import mu.KotlinLogging
 
 object MiwtterDatabaseRedisAdapter : MiwtterDatabase {
 
     private val logger = KotlinLogging.logger(this.javaClass.canonicalName)
 
-    private val usersList = mutableListOf<User>()
-    private val postsList = mutableListOf<Post>()
 
     init {
         logger.info("Local database created.")
     }
 
-    override fun registerUser(user: Miwtter.RegisterUserRequest): Boolean {
-        logger.info("Registering user [$user]")
-
-        return if(this.usersList.any { iterator -> iterator.name.contentEquals(user.name) }) {
-            false
-        } else {
-            val newUser = User(name = user.name, surname = user.surname, username = user.username, password = user.password)
-            this.usersList.add(newUser)
-        }
+    override fun registerUser(request: Miwtter.RegisterUserRequest): Miwtter.RegisterUserResponse {
+        TODO("Not yet implemented")
     }
 
-    override fun loginUser(user: LoginData): Boolean {
-        return this.usersList.any {
-                iterator -> iterator.username.equals(user.username) && iterator.password.contentEquals(user.password)
-        }
+    override fun loginUser(request: Miwtter.LoginUserRequest): Miwtter.LoginUserResponse {
+        TODO("Not yet implemented")
     }
 
-    override fun findUserByFreeText(freeText: String): List<User> {
-        return this.usersList.filter {
-            iterator -> iterator.name.contentEquals(freeText) || iterator.username.contentEquals(freeText)
-        }
+    override fun findUserByFreeText(request: Miwtter.FindUserRequest): Miwtter.FindUserResponse {
+        TODO("Not yet implemented")
     }
 
-    override fun createPost(post: CreatePostData): Boolean {
-        // 1. Check if the users exists.
-        val existsUser = this.usersList.any {
-            iterator -> iterator.username.contentEquals(post.ownerUsername)
-        }
-
-        return if(!existsUser) {
-            false
-        } else {
-            // 2. Add the post to the database.
-            this.postsList.add(Post(id = this.postsList.size.toString(), content = post.content, ownerUsername = post.ownerUsername))
-        }
+    override fun createPost(request: Miwtter.CreatePostRequest): Miwtter.CreatePostResponse {
+        TODO("Not yet implemented")
     }
 
-    override fun addLikeToPost(actorUsername: String, postId: String) {
-        // 1. Check that the posts exists
-        val postsExists = this.postsList.any {
-            iterator -> iterator.id.contentEquals(postId)
-        }
-
-        // 2. Check that the username exists
-        val userExists = this.usersList.any {
-                iterator -> iterator.username.contentEquals(actorUsername)
-        }
-
-        // 3. Add like to the post and the post to the user liked posts list.
-        if(postsExists && userExists) {
-            val post = postsList.find {
-                iterator -> iterator.id.contentEquals(postId)
-            }
-            val user = this.usersList.find {
-                iterator -> iterator.username.contentEquals(actorUsername)
-            }
-            if(user != null)
-                post?.likes = post?.likes?.plus(user)!!
-            if (post != null)
-                user?.likes = user?.likes?.plus(post)!!
-        }
-
+    override fun addLikeToPost(request: Miwtter.LikePostRequest): Miwtter.LikePostResponse {
+        TODO("Not yet implemented")
     }
 
-    override fun removeLike(actorUsername: String, postId: String) {
-        // 1. Check that the posts exists
-        val postsExists = this.postsList.any {
-                iterator -> iterator.id.contentEquals(postId)
-        }
-
-        // 2. Check that the username exists
-        val userExists = this.usersList.any {
-                iterator -> iterator.username.contentEquals(actorUsername)
-        }
-
-        // 3. Add like to the post and the post to the user liked posts list.
-        if(postsExists && userExists) {
-            val post = postsList.find {
-                    iterator -> iterator.id.contentEquals(postId)
-            }
-            val user = this.usersList.find {
-                    iterator -> iterator.username.contentEquals(actorUsername)
-            }
-            if(user != null)
-                post?.likes = post?.likes?.minus(user)!!
-            if (post != null)
-                user?.likes = user?.likes?.minus(post)!!
-        }
+    override fun removeLike(request: Miwtter.RemoveLikeRequest): Miwtter.RegisterUserResponse {
+        TODO("Not yet implemented")
     }
 
-    override fun getFeedForUser(username: String): List<Post> {
-        return this.postsList
+    override fun getFeedForUser(request: Miwtter.GetFeedRequest): Miwtter.GetFeedResponse {
+        TODO("Not yet implemented")
     }
 
-    override fun getUser(username: String): User? {
-        return this.usersList.find {
-            user -> user.username.contentEquals(username)
-        }
-    }
 
-    override fun getPost(postId: String): Post? {
-        return this.postsList.find {
-            post -> post.id.contentEquals(postId)
-        }
-    }
 }
