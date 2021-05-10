83b8e236057b3cf8dd448cc38c3c61c2035a1981
thewillyhuman <colunga91@gmail.com>
2021-05-10 08:23:31 +0200

    Local database implementation initialized

---
+++ src/miwtter-client-android/app/src/main/java/es/uniovi/miw/miwtter/persistence/LocalDatabase.kt
@@ -0,0 +1,36 @@
+package es.uniovi.miw.miwtter.persistence
+
+import es.uniovi.miw.miwtter.Miwtter
+
+interface LocalDatabase {
+
+    /**
+     * Gets a list containing all the posts in the local database.
+     */
+    fun getAll(): List<Miwtter.FeedPost>
+
+    /**
+     * Retrieves the corresponding post from the local database by its postId.
+     *
+     * @param postId is an string containing the id of the post to look for.
+     * @return the post that matched the given id if any. Otherwise null.
+     */
+    fun findByPostId(postId: String): Miwtter.FeedPost?
+
+    /**
+     * Adds the given post to the local database.
+     *
+     * @param post is a [Miwtter.FeedPost] that will be stored in the local database.
+     * @return true if the insert operation was successful. False otherwise.
+     */
+    fun addPost(post: Miwtter.FeedPost): Boolean
+
+    /**
+     * Removes a post from the local database based on its postId. If the post is not in the
+     * dabase hould return null.
+     *
+     * @param postId is the id of the post to remove.
+     * @return the post that was removed if it existed in the local database or null.
+     */
+    fun removePost(postId: String): Miwtter.FeedPost?
+}
