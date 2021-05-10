cd3e8b1155c0b6106f40a409bb516d83e1bc3113
mistermboy <pabloyo97@hotmail.com>
2021-05-10 15:38:01 +0200

    funcionalidad

--- src/miwtter-client-android/app/src/main/java/com/example/miwtter/adapters/FeedListAdapter.kt
+++ src/miwtter-client-android/app/src/main/java/com/example/miwtter/adapters/FeedListAdapter.kt
@@ -15,6 +15,7 @@ import com.like.LikeButton
 import com.like.OnLikeListener
 import es.uniovi.miw.miwtter.Miwtter
 import es.uniovi.miw.miwtter.clients.PostServiceClient
+import es.uniovi.miw.miwtter.clients.UsersServiceClient
 import es.uniovi.miw.miwtter.persistence.Settings
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
@@ -45,12 +46,29 @@ class FeedListAdapter(val items: List<Miwtter.FeedPost>) :
             .setContent(items[position].content)
             .setOwnerUsername(items[position].ownerUsername)
             .build()
+
         GlobalScope.async  {
             if(PostsList.requestFavPosts().contains(post)) {
                 holder.cardView.findViewById<LikeButton>(R.id.star_button).isLiked = true
             }
         }
 
+        val userRequest  = Miwtter.FindUserRequest.newBuilder()
+            .setUsername(Settings(App.instance).username)
+            .setFindPolicy(Miwtter.FindUserRequest.FindPolicy.AND)
+            .build()
+
+
+        val userClient = UsersServiceClient
+        val response = userClient.find(userRequest)
+        var posts  =response.userList.get(0).likedPostsList
+        posts = posts.filter {
+            p-> p.postId.contentEquals(post.postId)
+        }
+        if(!posts.isEmpty()) {
+            holder.cardView.findViewById<LikeButton>(R.id.like_button).isLiked = true
+            //holder.cardView.findViewById<TextView>(R.id.num_likes).text = Integer.toString(items[position].numberOfLikes+1)
+        }
 
         holder.cardView.findViewById<LikeButton>(R.id.like_button).setOnLikeListener(object : OnLikeListener {
             override fun liked(likeButton: LikeButton) {
@@ -60,7 +78,8 @@ class FeedListAdapter(val items: List<Miwtter.FeedPost>) :
                     .setPostId(items[position].postId)
                     .build()
                 service.addLike(request)
-                holder.cardView.findViewById<TextView>(R.id.num_likes).text = Integer.toString(items[position].numberOfLikes+1)
+                var numLikes = holder.cardView.findViewById<TextView>(R.id.num_likes).text
+                holder.cardView.findViewById<TextView>(R.id.num_likes).text = Integer.toString(Integer.parseInt(numLikes.toString()) +1)
             }
             override fun unLiked(likeButton: LikeButton) {
                 val service = PostServiceClient
@@ -69,28 +88,16 @@ class FeedListAdapter(val items: List<Miwtter.FeedPost>) :
                     .setPostId(items[position].postId)
                     .build()
                 service.removeLike(request)
-                holder.cardView.findViewById<TextView>(R.id.num_likes).text = Integer.toString(items[position].numberOfLikes)
+                var numLikes = holder.cardView.findViewById<TextView>(R.id.num_likes).text
+                holder.cardView.findViewById<TextView>(R.id.num_likes).text = Integer.toString(Integer.parseInt(numLikes.toString()) -1)
             }
         })
 
         holder.cardView.findViewById<LikeButton>(R.id.star_button).setOnLikeListener(object : OnLikeListener {
             override fun liked(likeButton: LikeButton) {
-                val post = Miwtter.FeedPost.newBuilder()
-                    .setOwnerName(items[position].ownerName)
-                    .setPostId(items[position].postId)
-                    .setContent(items[position].content)
-                    .setOwnerUsername(items[position].ownerUsername)
-                    .build()
-
                 GlobalScope.async  {  PostsList.createFavPost(post)}
             }
             override fun unLiked(likeButton: LikeButton) {
-                val post = Miwtter.FeedPost.newBuilder()
-                    .setOwnerName(items[position].ownerName)
-                    .setPostId(items[position].postId)
-                    .setContent(items[position].content)
-                    .setOwnerUsername(items[position].ownerUsername)
-                    .build()
                  GlobalScope.async {
                      PostsList.removeFavPost(post)
                  }
